bc0d680cc1211dc881e505e84c24d8260af4e11a
ZAMBUR <136861183+SNABUR@users.noreply.github.com>
2024-10-13 08:43:15 -0500

    Add files via upload

---
+++ app_Memes/abis/Constants.js
@@ -0,0 +1,7 @@
+const UniswapV2Factory = require('./UniswapV2Factory.json');
+
+const contractABI_UNISWAP_FACTORY_V2 = UniswapV2Factory.abi;
+
+module.exports = {
+    contractABI_UNISWAP_FACTORY_V2: contractABI_UNISWAP_FACTORY_V2
+};
